e8d9b9b1714b78bb6a45001cde50596a6e781fca
Allagan Software Deployment Node <asdn@azys.la>
2020-03-12 12:27:53 +0900

    circleci

---
+++ .circleci/deploy.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+git config --global user.email "asdn@azys.la"
+git config --global user.name "Allagan Software Deployment Node"
+
+git clone git@github.com:davidsiaw/ffxiv-craft-opt-web.git build
+pushd build
+  git checkout gh-pages
+popd
+cp -r build/.git ./gittemp
+rm -rf build
+cp -r app build
+cp -r ./gittemp build/.git
+pushd build
+  echo crafttool.outsider.azys.la > CNAME
+  git add .
+  git add -u
+  git commit -m "update `date`"
+  ssh-agent bash -c 'ssh-add ~/.ssh/id_github.com; git push'
+popd
